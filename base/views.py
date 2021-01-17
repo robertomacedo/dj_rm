3f878bb902367a34bcad352baeb7cd75c4d1bc7f
robertomacedo <macedorss20@gmail.com>
2021-01-16 23:41:29 -0300

    Criada app olá mundo
    #close 9

--- base/views.py
+++ base/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Olá mundo')
+    return HttpResponse('<h1>Bem Vindo page initial</h1>')
