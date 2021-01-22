fa52f5fc66f6562e636370d69f7d235b8cb3afb0
robertomacedo <macedorss20@gmail.com>
2021-01-21 22:20:22 -0300

    Configurada cobertura de código

--- django_rm/base/views.py
+++ django_rm/base/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Bem Vindo page initial</h1>')
+    return HttpResponse('<h1>Python Pro curso de Django</h1>')
