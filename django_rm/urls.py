b7f0551fec884ca31dc9d8ab1a2b07d364c41d29
robertomacedo <macedorss20@gmail.com>
2021-01-21 21:34:59 -0300

    Corrigido erro em urls

--- django_rm/urls.py
+++ django_rm/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django_rm.base import home
+from django_rm.base.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
