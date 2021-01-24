235c02a76cc3d34e3fbcbf7580e4ff5fb0e99526
robertomacedo <macedorss20@gmail.com>
2021-01-24 15:31:07 -0300

    alterado pipfile

--- django_rm/settings.py
+++ django_rm/settings.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+from functools import partial
 from pathlib import Path
-
+import dj_database_url
 from decouple import config
+import os.path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hal%&tn$qq%)$9fa+tdkbjkm_q7pt-9d)o139+i)c((bk5bl(r'
+SECRET_KEY = config('SECRET_KEY') #'hal%&tn$qq%)$9fa+tdkbjkm_q7pt-9d)o139+i)c((bk5bl(r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
@@ -77,12 +78,13 @@ WSGI_APPLICATION = 'django_rm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+default_db_url = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+parse_database = partial(dj_database_url.parse, conn_max_age=600)
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': config('DATABASE_URL', default=default_db_url, cast=parse_database)
     }
-}
 
 
 # Password validation
