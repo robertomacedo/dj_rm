08ba202d109be37e5c1828794882c68e0ba25f05
robertomacedo <macedorss20@gmail.com>
2021-01-28 12:07:09 -0300

    instalada a lib collectfast

--- django_rm/settings.py
+++ django_rm/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'collectfast',
     'django.contrib.staticfiles',
     # My apps
     'django_rm.base',
@@ -130,6 +131,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
+COLLECTFAST_ENABLE = False
+
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 
 # Sotorage config in s3 AWS
@@ -141,8 +144,10 @@ if AWS_ACCESS_KEY_ID:
     AWS_PRELOAD_METADATA = True
     AWS_AUTO_CREATE_BUCKET = False
     AWS_QUERYSTRING_AUTH = True
-    #AWS_S3_CUSTOM_DOMAIN = None
-    #AWS_DEFAULT_ACL = 'private'
+    AWS_S3_CUSTOM_DOMAIN = None #ver depois
+    AWS_DEFAULT_ACL = 'private' # ver depois
+
+    COLLECTFAST_ENABLE = True
 
     AWS_DEFAULT_ACL = None
 
