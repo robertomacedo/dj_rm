1dbcd8d82f9eb06bb05cb97ddb51bf4aff17d715
robertomacedo <macedorss20@gmail.com>
2021-01-28 12:39:41 -0300

    alteração no django-admin

--- django_rm/base/admin.py
+++ django_rm/base/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 
-from .models import Membro, Doc
+#from .models import Membro, Doc
 
 
-@admin.register(Membro)
-class PostMembro(admin.ModelAdmin):
-    list_display = ("name", "phone_namber", "cpf", "conjuge", "created", "updated")
+#@admin.register(Membro)
+#class PostMembro(admin.ModelAdmin):
+#    list_display = ("name", "phone_namber", "cpf", "conjuge", "created", "updated")
 
-@admin.register(Doc)
-class DocsMembro(admin.ModelAdmin):
-    list_display = ("cpf", "rg", "titulo_eleitor", "created", "updated")
+#@admin.register(Doc)
+#class DocsMembro(admin.ModelAdmin):
+#    list_display = ("cpf", "rg", "titulo_eleitor", "created", "updated")
