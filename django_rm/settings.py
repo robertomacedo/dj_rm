647e34629fab3215a96bdbd6eef0f794b5b46560
robertomacedo <macedorss20@gmail.com>
2021-01-28 21:25:31 -0300

    inserido url em allowed_hosts

--- django_rm/settings.py
+++ django_rm/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY') #'hal%&tn$qq%)$9fa+tdkbjkm_q7pt-9d)o139+i)c((b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pythondjangorm.herokuapp.com/']
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
