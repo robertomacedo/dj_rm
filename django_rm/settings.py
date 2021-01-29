943ad0c65fa9d70d10f8e3da62c333c492f4e4c1
robertomacedo <macedorss20@gmail.com>
2021-01-28 22:00:04 -0300

    midificado configuração aws no settings

--- django_rm/settings.py
+++ django_rm/settings.py
@@ -146,8 +146,8 @@ if AWS_ACCESS_KEY_ID:
     AWS_PRELOAD_METADATA = True
     AWS_AUTO_CREATE_BUCKET = False
     AWS_QUERYSTRING_AUTH = True
-    AWS_S3_CUSTOM_DOMAIN = None #ver depois
-    AWS_DEFAULT_ACL = 'private' # ver depois
+    #AWS_S3_CUSTOM_DOMAIN = None #ver depois
+    #AWS_DEFAULT_ACL = 'private' # ver depois
 
     COLLECTFAST_ENABLE = True
 
